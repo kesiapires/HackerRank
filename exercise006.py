49a4113b4135d4ee10e9eab736c70c5cb24f4e8a
Kesia Pires <kesiapiressantos@gmail.com>
2024-04-30 14:11:29 +0100

    single number using Xor - LeetCode

---
+++ exercise006.py
@@ -0,0 +1,9 @@
+#Single Number
+#LeetCode
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for i in nums:
+            result ^= i
+        return result
